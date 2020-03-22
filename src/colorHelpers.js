60cca8f9402523cfe21204734e05394bbb168e27
Fernando <f.sanchezvilela@mrmilu.com>
2020-03-22 16:50:28 +0100

    Add unstyled color slider

--- src/colorHelpers.js
+++ src/colorHelpers.js
@@ -1,30 +1,29 @@
 import chroma from 'chroma-js';
 const levels = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
 
-function generatePallette(starterPallet) {
+function generatePalette(starterPalette) {
   let newPalette = {
     paletteName: starterPalette.paletteName,
-    id: starterPallet.id,
-    emoji: starterPallet.emoji,
+    id: starterPalette.id,
+    emoji: starterPalette.emoji,
     colors: {}
-  }
+  };
   for (let level of levels) {
     newPalette.colors[level] = [];
   }
-  for (let color of starterPallet.colors) {
+  for (let color of starterPalette.colors) {
     let scale = getScale(color.color, 10).reverse();
     for (let i in scale) {
-      newPalette.colors[
-        levels[i].push({
+      newPalette.colors[levels[i]].push({
           name: `${color.name} ${levels[i]}`,
           id: color.name.toLowerCase().replace(/ /g, '-'),
           hex: scale[i],
           rgb: chroma(scale[i]).css(),
-          rgba: chroma(scale[i]).css()
-            .replace("rgb", "rgba")
-            .replace(")", ",1.0)"),
+          rgba: chroma(scale[i])
+            .css()
+            .replace('rgb', 'rgba')
+            .replace(')', ',1.0)')
         })
-      ];
     }
   }
   return newPalette;
@@ -46,4 +45,4 @@ function getScale(hexColor, numberOfColors) {
     .colors(numberOfColors);
 }
 
-export default { generatePallette };
+export { generatePalette };
