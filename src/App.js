22b9474cd45761e9456440f83805533cd7c3ffd5
Fernando <f.sanchezvilela@mrmilu.com>
2020-04-11 16:44:17 +0200

    Add Link to ColorBox and Add Route

--- src/App.js
+++ src/App.js
@@ -31,6 +31,10 @@ class App extends Component {
             />
           )}
         />
+        <Route
+          exact
+          path="/palette/:paletteId/:colorId"
+          render={() => <h1>SINGLE COLOR PAGE!</h1>} />
       </Switch>
     );
   }
