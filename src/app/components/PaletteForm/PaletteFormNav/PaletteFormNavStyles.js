f4d711671cc20c9969b8dafa7400c991816f9556
Fernando <f.sanchezvilela@mrmilu.com>
2020-06-20 15:29:23 +0200

    Tweak Style form

--- src/app/components/PaletteForm/PaletteFormNav/PaletteFormNavStyles.js
+++ src/app/components/PaletteForm/PaletteFormNav/PaletteFormNavStyles.js
@@ -5,11 +5,17 @@ const styles = (theme) => ({
     root: {
       display: 'flex',
     },
+    hide:{
+      display:'none',
+    },
     appBar: {
       transition: theme.transitions.create(['margin', 'width'], {
         easing: theme.transitions.easing.sharp,
         duration: theme.transitions.duration.leavingScreen,
+        
       }),
+      background:'white',
+      color: "rgba(0,0,0,0.8)",
       flexDirection: 'row',
       justifyContent: 'space-between',
       alignItems: "center",
