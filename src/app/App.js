7f6bc06166abd2533439c43cea0ee122c346c1d4
Fernando <f.sanchezvilela@mrmilu.com>
2020-06-20 15:40:19 +0200

    save to local storage

--- src/app/App.js
+++ src/app/App.js
@@ -12,7 +12,9 @@ import { generatePalette } from './utils/colorHelpers';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { palettes: seedColors };
+    //LOCAL STORAGE;
+    const savedPalettes = JSON.parse(window.localStorage.getItem('palettes'));
+    this.state = { palettes: savedPalettes || seedColors };
     this.savePalette = this.savePalette.bind(this);
     this.findPalette = this.findPalette.bind(this);
   }
@@ -22,7 +24,15 @@ class App extends Component {
     });
   }
   savePalette(newPalette) {
-    this.setState({ palettes: [...this.state.palettes, newPalette] });
+    this.setState({ palettes: [...this.state.palettes, newPalette] }, this.syncLocalStorage);
+    
+  }
+  syncLocalStorage() {
+    // save palettes to local storages
+    window.localStorage.setItem(
+      'palettes',
+      JSON.stringify(this.state.palettes)
+    );
   }
   render() {
     return (
