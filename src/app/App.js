f30d24cbb9151c3a3fb1bb709bfe956048a7b97d
Fernando <f.sanchezvilela@mrmilu.com>
2020-08-11 20:29:15 +0200

    Prevent Duplicate Random Colors

--- src/app/App.js
+++ src/app/App.js
@@ -106,6 +106,15 @@ class App extends Component {
                 </Page>
               )}
             />
+            <Route render={(routeProps) => (
+                <Page>
+                  <PaletteList 
+                    palettes={this.state.palettes}
+                    deletePalette={this.deletePalette}
+                    {...routeProps} />
+                </Page>
+              )}
+            />
           </Switch>
           </CSSTransition>
         </TransitionGroup>
