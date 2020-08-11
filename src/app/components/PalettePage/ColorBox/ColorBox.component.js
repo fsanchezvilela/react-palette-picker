378291e804a198b3dfb7445a40416536792b316f
Fernando <f.sanchezvilela@mrmilu.com>
2020-08-11 19:59:25 +0200

    Fix navbar slider size and add classnames to colorbox

--- src/app/components/PalettePage/ColorBox/ColorBox.component.js
+++ src/app/components/PalettePage/ColorBox/ColorBox.component.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { Link } from 'react-router-dom';
-
+import classNames from 'classnames';
 import { withStyles } from '@material-ui/core';
-import styles from "./ColorBoxStyles";
+import styles from './ColorBoxStyles';
 
 class ColorBox extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      copied: false
+      copied: false,
     };
     this.changeCopyState = this.changeCopyState.bind(this);
   }
@@ -21,31 +21,43 @@ class ColorBox extends Component {
   }
 
   render() {
-    const { name, background, moreUrl, showingFullPalette, classes} = this.props;
+    const {
+      name,
+      background,
+      moreUrl,
+      showingFullPalette,
+      classes,
+    } = this.props;
     const { copied } = this.state;
 
     return (
-      <CopyToClipboard text={background} onCopy={ this.changeCopyState }>
+      <CopyToClipboard text={background} onCopy={this.changeCopyState}>
         <div style={{ background }} className={classes.ColorBox}>
           <div
             style={{ background }}
-            className={`${classes.copyOverlay} ${copied && classes.showOverlay}`}
+            className={classNames(classes.copyOverlay, {
+              [classes.showOverlay]: copied,
+            })}
           />
-          <div className={`${classes.copyMessage} ${copied && classes.copyMessageShow}`}>
+          <div
+            className={classNames(classes.copyMessage, {
+              [classes.copyMessageShow]: copied,
+            })}
+          >
             <h1>copied!</h1>
-            <p className={ classes.copyText }>{background}</p>
+            <p className={classes.copyText}>{background}</p>
           </div>
           <div>
-            <div className={ classes.boxContent }>
-              <span className={ classes.colorName }>{name}</span>
+            <div className={classes.boxContent}>
+              <span className={classes.colorName}>{name}</span>
             </div>
-            <button className={ classes.copyButton }>Copy</button>
+            <button className={classes.copyButton}>Copy</button>
           </div>
-          {showingFullPalette &&   
-          <Link to={moreUrl} onClick={(e)=> e.stopPropagation()}>
-            <span className={classes.seeMore}>MORE</span>
-          </Link>
-          }
+          {showingFullPalette && (
+            <Link to={moreUrl} onClick={(e) => e.stopPropagation()}>
+              <span className={classes.seeMore}>MORE</span>
+            </Link>
+          )}
         </div>
       </CopyToClipboard>
     );
