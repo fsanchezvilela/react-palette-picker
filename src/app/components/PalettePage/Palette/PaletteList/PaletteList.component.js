e4d081634b037dcaaa7a44734d948cf3f97464e2
Fernando <f.sanchezvilela@mrmilu.com>
2020-08-11 20:43:23 +0200

    fix duplicate color and name

--- src/app/components/PalettePage/Palette/PaletteList/PaletteList.component.js
+++ src/app/components/PalettePage/Palette/PaletteList/PaletteList.component.js
@@ -49,7 +49,7 @@ class PaletteList extends Component {
       <div className={classes.root}>
         <div className={classes.container}>
           <nav className={classes.nav}>
-            <h1 className={classes.heading}>ReactColors</h1>
+            <h1 className={classes.heading}>Colorette</h1>
             <Link to="/palette/new">Create Palette</Link>
           </nav>
             <TransitionGroup className={classes.palettes}>
